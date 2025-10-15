7d15ef9accc9b6149a2c88ecf03a07ae827b2174
mehrzadnik <mehrzadnikkhahbahrami@gmail.com>
2025-10-15 02:23:54 -0700

    Add files via upload

---
+++ tamrin1 daneshghah/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamrin1_daneshghah
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("enter number");
+            double r = double.Parse(Console.ReadLine());
+            double mohit = 2 * 3.14 * r;
+            double masahat = 3.14 * r * r;
+
+            Console.WriteLine("mohit=" +mohit);
+            Console.WriteLine("masahat=" +masahat);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ tamrin2 daneshgah/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamrin2_daneshgah
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter temperature=");
+            double c = double.Parse(Console.ReadLine());
+            double f = (c * 9 / 5) + 32;
+            Console.WriteLine("temperature=" + f);
+            Console.ReadLine();
+
+        }
+    }
+}

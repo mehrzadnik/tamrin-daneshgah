f0429516a1169ba5244dc2db3d97090e45c48686
mehrzadnik <mehrzadnikkhahbahrami@gmail.com>
2025-10-23 09:50:21 -0700

    Add files via upload

---
+++ tamrin3 daneshgah/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamrin3_daneshgah
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("enter your number:");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            if (a < 10 && a%2==0)
+            {
+                Console.Write("TRUE!");
+            }
+            else
+            {
+                Console.Write("Your number is greater than 10 or its an  odd number :(");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ tamrin4 daneshgah/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamrin4_daneshgah
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("enter your number:");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            if (a % 7 == 0) 
+            {
+                Console.Write("yes!");
+            }
+             else
+            {
+                a = a * 3;
+                Console.Write($"result is:{a}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

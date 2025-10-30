5704dc442785b3ad3fb54d16c9ef7f2136b1593b
mehrzadnik <mehrzadnikkhahbahrami@gmail.com>
2025-10-30 13:56:39 -0700

    Add files via upload

---
+++ tamrin5 daneshgah/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamrin5_daneshgah
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter your number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            switch (a)
+            {
+                case 1:
+
+                    Console.WriteLine("Monday");
+                    break;
+
+                case 2:
+
+                    Console.WriteLine("Tuesday");
+                    break;
+
+                case 3:
+
+                    Console.WriteLine("Wednesday");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+
+                default:
+                    Console.WriteLine("end!");
+                    break;
+            }
+
+          Console.ReadKey();
+        }
+    }
+    }
---
+++ tamrin7 daneshgah/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamrin7_daneshgah
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a, sum = 1;
+            Console.Write("enter your number:");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            while(a!=0)
+            {
+                sum *= a;
+                Console.Write("enter your number:");
+                a = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("sum:{0}", sum);
+            Console.ReadKey();
+        }
+    }
+}

66cfcb8a384e1551004050195d11a2a70b684d92
mehrzadnik <mehrzadnikkhahbahrami@gmail.com>
2025-11-21 11:04:23 -0800

    Add files via upload

---
+++ tamrin11daneshgah/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamrin11daneshgah
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // تابع بازگشتی برای ضرب دو عدد
+            static int Multiply(int a, int b)
+            {
+                // اگر یکی از اعداد صفر باشد
+                if (b == 0)
+                    return 0;
+
+                // اگر b منفی باشد
+                if (b < 0)
+                    return -Multiply(a, -b);
+
+                // حالت بازگشتی (a + a + ... b بار)
+                return a + Multiply(a, b - 1);
+            }
+
+            static void Main()
+            {
+                Console.Write("Enter number a: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter number b: ");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                int result = Multiply(a, b);
+
+                Console.WriteLine("Multiplication Result = " + result);
+            }
+        }
+    }
+    
+}
---
+++ tamrindaneshgah10/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamrindaneshgah10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // متد محاسبه محیط مثلث
+            static double Perimeter(double a, double b, double c)
+            {
+                return a + b + c;
+            }
+
+            // متد محاسبه مساحت مثلث (فرمول هرون)
+            static double Area(double a, double b, double c)
+            {
+                double s = (a + b + c) / 2; // نصف محیط
+                return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+            }
+
+            static void Main()
+            {
+                Console.Write("Enter side a: ");
+                double a = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter side b: ");
+                double b = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter side c: ");
+                double c = Convert.ToDouble(Console.ReadLine());
+
+                double perimeter = Perimeter(a, b, c);
+                double area = Area(a, b, c);
+
+                Console.WriteLine("Perimeter of triangle = " + perimeter);
+                Console.WriteLine("Area of triangle = " + area);
+
+            }
+        }
+        
+    }
+   
+}
